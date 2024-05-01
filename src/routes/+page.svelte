4692581aebbd01c61e907fcd536b87bd2633dc54
Johnathan Stevers <jmstevers@gmail.com>
2024-05-01 12:52:27 -0500

    Revert "car ride"
    This reverts commit 685374f28337523f175bf92afde83cd45cba14c8.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,9 @@
     import { Button } from "$lib/components/ui/button";
     import { invoke } from "@tauri-apps/api/core";
     import { emit } from "@tauri-apps/api/event";
-    import { Square, Circle } from "lucide-svelte";
-
-    let recording = $state(false);
+    import { Square } from "lucide-svelte";
 </script>
 
 <Button onclick={async () => await invoke("create_window")}>Open Window</Button>
 <Button onclick={async () => await invoke("play_sound")}>Play Sound</Button>
-<Button onclick={async () => await invoke("record_sound")}>
-    <Square />
-</Button>
+<Button onclick={async () => await invoke("record_sound")}><Square /></Button>
