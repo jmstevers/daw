685374f28337523f175bf92afde83cd45cba14c8
Johnathan Stevers <jmstevers@gmail.com>
2024-04-30 21:00:28 -0500

    car ride

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,13 @@
     import { Button } from "$lib/components/ui/button";
     import { invoke } from "@tauri-apps/api/core";
     import { emit } from "@tauri-apps/api/event";
-    import { Square } from "lucide-svelte";
+    import { Square, Circle } from "lucide-svelte";
+
+    let recording = $state(false);
 </script>
 
 <Button onclick={async () => await invoke("create_window")}>Open Window</Button>
 <Button onclick={async () => await invoke("play_sound")}>Play Sound</Button>
-<Button onclick={async () => await invoke("record_sound")}><Square /></Button>
+<Button onclick={async () => await invoke("record_sound")}>
+    <Square />
+</Button>
